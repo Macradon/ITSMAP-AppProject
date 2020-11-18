16e6b33cfcb16230dfd5aaa7faf6135f6608f699
Macradon <nguyen.tri0c@gmail.com>
2020-11-18 22:36:18 +0100

    Database elements and Models set up

---
+++ PlanSwapApplication/app/src/main/java/com/au564065/plantswap/database/PlantDAO.java
@@ -0,0 +1,8 @@
+package com.au564065.plantswap.database;
+
+import androidx.room.Dao;
+
+//DAO implementation adapted from course lecture and https://google-developer-training.github.io/android-developer-advanced-course-practicals/unit-6-working-with-architecture-components/lesson-14-room,-livedata,-viewmodel/14-1-a-room-livedata-viewmodel/14-1-a-room-livedata-viewmodel.html#appintro
+@Dao
+public interface PlantDAO {
+}
--- PlanSwapApplication/app/src/main/java/com/au564065/plantswap/database/Repository.java
+++ PlanSwapApplication/app/src/main/java/com/au564065/plantswap/database/Repository.java
@@ -1,4 +1,33 @@
 package com.au564065.plantswap.database;
 
+import androidx.lifecycle.LiveData;
+
+import com.au564065.plantswap.models.Plant;
+import com.au564065.plantswap.models.Swap;
+
+import java.util.List;
+
+//Singleton repository code adapted from lecture 7 Tracker application
 public class Repository {
+
+    //Singleton Repository instance
+    private static Repository INSTANCE = null;
+
+    //Cached list of plants in WishList
+    private static LiveData<List<Plant>> WishList;
+    //Cached list of plants for swap
+    private static LiveData<List<Swap>> SwapList;
+    //Cached search history
+    private static LiveData<List<Plant>> SearchHistory;
+
+    //Empty Repository constructor that will never be called
+    private Repository(){
+
+    }
+
+    //Method returns an instance of Repository and lazyloads the instance on first call.
+    public static Repository getInstance() {
+        if (INSTANCE==null) INSTANCE = new Repository();
+        return INSTANCE;
+    }
 }
