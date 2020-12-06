f4ea294d9a95872ece7a0c3622d3a0ed3ab78da4
Macradon <nguyen.tri0c@gmail.com>
2020-12-06 14:02:00 +0100

    Message update with SenderId

--- PlantSwapApplication/app/src/main/java/com/au564065/plantswap/models/Message.java
+++ PlantSwapApplication/app/src/main/java/com/au564065/plantswap/models/Message.java
@@ -3,10 +3,12 @@ package com.au564065.plantswap.models;
 public class Message {
     private String name;
     private String message;
+    private String senderId;
 
-    public Message(String name, String message) {
+    public Message(String name, String message, String senderId) {
         this.name = name;
         this.message = message;
+        this.senderId = senderId;
     }
 
     public String getName() {
@@ -23,5 +25,13 @@ public class Message {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
     }
 }
